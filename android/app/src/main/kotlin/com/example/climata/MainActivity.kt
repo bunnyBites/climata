d838c5a85f8156197e084f4c7c12ba8728b641c5
bunnyBites <s.mukeshkr481@gmail.com>
2023-05-17 14:33:27 +0530

    initial setup

---
+++ android/app/src/main/kotlin/com/example/climata/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.climata
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Climata',
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Climata"),
+      ),
+      body: const SafeArea(child: Text("hello")),
+    );
+  }
+}
